af25d7f9289f44039df6c1210e1998a3f89eb699
Estelle <estellemol11@gmail.com>
2019-06-21 20:58:37 +0200

    mail-check exo

---
+++ mail-check/email.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+
+    function emailIsValid (email) {
+        let regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regx.test(String(email).toLowerCase());
+    }
+
+    console.log(emailIsValid('estellemol-@@gmail.com')) 
+
+    /* let userEmail = prompt('Enter your email');
+
+    for(i=2; i<userEmail[15]; i++){
+        console.log(userEmail);
+    } */
+
+}
